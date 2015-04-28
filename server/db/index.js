58310fa34401c7ce0cd03a0cb1739a6edb0925d0
thejsj <jorge.silva@thejsj.com>
2015-04-28 11:32:33 -0700

    Switching to rethinkdb-init

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,7 @@
 
 var q = require('q');
 var r = require('rethinkdb');
+require('rethinkdb-init')(r);
 var config = require('config');
 
 r.connections = [];
@@ -15,54 +16,21 @@ r.getNewConnection = function () {
     });
 };
 
-var createIfDoesntExist = function (query) {
-  return query
-    .run(r.conn)
-    .catch(function () {});
-};
-
-r.connect(config.get('rethinkdb'))
-  .then(function (conn) {
-    r.conn = conn;
-    r.connections.push(conn);
-    return r.dbCreate(config.get('rethinkdb').db).run(r.conn)
-      .then(function () {})
-      .catch(function () {})
-      .then(function () {
-        r.conn.use(config.get('rethinkdb').db);
-        // Create Tables
-        return q()
-          // Create tables
-          .then(function () {
-            return r.tableCreate('images');
-          })
-          .then(createIfDoesntExist)
-          .then(function () {
-            return r.tableCreate('comments');
-          })
-          .then(createIfDoesntExist)
-          .then(function () {
-            return r.tableCreate('users');
-          })
-          .then(createIfDoesntExist)
-          // Create Indexes
-          .then(function () {
-            return r.table('images').indexCreate('createdAt');
-          })
-          .then(createIfDoesntExist)
-          .then(function () {
-            return r.table('comments').indexCreate('createdAt');
-          })
-          .then(createIfDoesntExist)
-          .then(function () {
-            return r.table('comments').indexCreate('imageId');
-          })
-          .then(createIfDoesntExist)
-          .then(function () {
-            return r.table('users').indexCreate('login');
-          })
-          .then(createIfDoesntExist);
-      });
-  });
+r.init(config.get('rethinkdb'), [
+  {
+    name: 'images',
+    indexes: ['createdAt']
+  }, {
+    name: 'comments',
+    indexes: ['createdAt', 'imageId']
+  }, {
+    name: 'users',
+    indexes: ['login']
+  }
+]).then(function (conn) {
+  r.conn = conn;
+  r.connections.push(conn);
+  r.conn.use(config.get('rethinkdb').db);
+});
 
 module.exports = r;
